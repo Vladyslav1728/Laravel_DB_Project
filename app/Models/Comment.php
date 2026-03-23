06feb998982eb47485a8be9da5bf7ccb352f134a
Vladyslav Shcherbyna <158246943+Vladyslav1728@users.noreply.github.com>
2026-03-23 09:10:43 +0100

    Cvicenie 6: Models - Task + Comment

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'body',
+    ];
+
+    // tu budú relácie
+}
